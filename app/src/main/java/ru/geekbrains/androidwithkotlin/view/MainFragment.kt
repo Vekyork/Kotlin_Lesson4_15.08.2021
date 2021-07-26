020c3e06861dd8c3db1300e758417309705aa62f
DwarfPorter <mrvlmor@gmail.com>
2021-07-26 21:53:34 +0300

    Добавление AppState

--- app/src/main/java/ru/geekbrains/androidwithkotlin/view/MainFragment.kt
+++ app/src/main/java/ru/geekbrains/androidwithkotlin/view/MainFragment.kt
@@ -10,7 +10,9 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import ru.geekbrains.androidwithkotlin.R
 import ru.geekbrains.androidwithkotlin.databinding.MainFragmentBinding
+import ru.geekbrains.androidwithkotlin.model.AppState
 import ru.geekbrains.androidwithkotlin.viewmodel.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class MainFragment : Fragment() {
 
@@ -42,7 +44,7 @@ class MainFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val observer = Observer<String> { a ->
+        val observer = Observer<AppState> { a ->
             renderData(a)
         }
         viewModel.getData().observe(viewLifecycleOwner, observer)
@@ -51,8 +53,23 @@ class MainFragment : Fragment() {
         }
     }
 
-    private fun renderData(data: String) {
-        binding.message.text = data
+    private fun renderData(data: AppState) {
+        when(data){
+            is AppState.Success -> {
+                val weatherData = data.weatherData
+                binding.loadingLayout.visibility = View.GONE
+                binding.message.text = weatherData
+            }
+            is AppState.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE
+            }
+            is AppState.Error -> {
+                binding.loadingLayout.visibility = View.GONE
+                Snackbar.make(binding.main, "Error", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Reload") {viewModel.requestData(binding.edit.text.toString())}
+                    .show()
+            }
+        }
     }
 
 }
