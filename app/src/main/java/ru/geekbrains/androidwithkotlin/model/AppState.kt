f50524e0581065a24cd45fdd6b8d598ccecc63f2
DwarfPorter <mrvlmor@gmail.com>
2021-07-29 22:10:48 +0300

    Основной экран - список

--- app/src/main/java/ru/geekbrains/androidwithkotlin/model/AppState.kt
+++ app/src/main/java/ru/geekbrains/androidwithkotlin/model/AppState.kt
@@ -3,7 +3,7 @@ package ru.geekbrains.androidwithkotlin.model
 import ru.geekbrains.androidwithkotlin.model.data.Weather
 
 sealed class AppState {
-    data class Success(val weatherData: Weather) : AppState()
+    data class Success(val weatherData: List<Weather>) : AppState()
     class Error(val error: Throwable) : AppState()
     object Loading : AppState()
 }
