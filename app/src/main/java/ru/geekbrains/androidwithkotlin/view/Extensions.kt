8a4ab651180084bc662d0b7dff01e66d481ced72
Vekyork <vekyork@yandex.ru>
2021-08-15 17:33:12 +0300

    Расширил возможности SnackBar

--- app/src/main/java/ru/geekbrains/androidwithkotlin/view/Extensions.kt
+++ app/src/main/java/ru/geekbrains/androidwithkotlin/view/Extensions.kt
@@ -1,8 +1,10 @@
 package ru.geekbrains.androidwithkotlin.view
 
 import android.view.View
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_INDEFINITE
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_LONG
 import com.google.android.material.snackbar.Snackbar
-
+//Показать SnackBar c передачей текста в виде String
 fun View.showSnackBar(text: String, actionText: String, action: (View) -> Unit){
     Snackbar.make(this, text, Snackbar.LENGTH_INDEFINITE)
         .setAction(actionText, action).show()
@@ -20,4 +22,9 @@ fun View.hide(): View{
         visibility = View.GONE
     }
     return this
+
+    //Показать SnackBar c отображением строкового ресурса из R.String
+    fun View.showSnackBar(text: String, actionText: String, action: (View) -> Unit){
+        Snackbar.make(this, context.getString(LENGTH_LONG), LENGTH_LONG).show()
+    }
 }
